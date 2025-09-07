3cb004b7985ae018c0f80485633e6b47a3d25f4d
Ahmet Tarik Duyar(Atduyar) <atduyar@gmail.com>
2025-09-07 23:11:24 +0300

    add: Store, terminal and some other plugins

---
+++ lua/plugins/toggleterm.nvim.lua
@@ -0,0 +1,13 @@
+-- Plugin: akinsho/toggleterm.nvim
+-- Installed via store.nvim
+
+return {
+	'akinsho/toggleterm.nvim',
+	version = "*",
+	config = true,
+	opts = {
+
+		open_mapping = [[<c-\>]], -- or { [[<c-\>]], [[<c-¥>]] } if you also use a Japanese keyboard.
+	}
+}
+
---
+++ lua/plugins/vim-moonfly-colors.lua
@@ -0,0 +1,9 @@
+-- Plugin: bluz71/vim-moonfly-colors
+-- Installed via store.nvim
+
+return {
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    lazy = false,
+    priority = 1000
+}
